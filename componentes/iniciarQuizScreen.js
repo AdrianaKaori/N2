80389f38f00924ea27ab12e82810f371b37bf847
AdrianaKaori <adriana.kakazu@gmail.com>
2025-09-23 17:54:05 -0300

    correções

--- componentes/iniciarQuizScreen.js
+++ componentes/iniciarQuizScreen.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { View, Text, Button, Alert, Picker } from 'react-native';
-import { getTemasComQuantidadePerguntas } from '../database/dbTemas';
 
 export default function IniciarQuizScreen({ navigation }) {
   const [temas, setTemas] = useState([]);
